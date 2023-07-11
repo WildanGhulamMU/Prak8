c293a1902401771c916ab8d9717a64420687cf12
WildanGhulamMU <wildanghulam@students.amikom.ac.id>
2023-07-11 18:52:05 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿﻿using Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Wildan Ghulam Muhammad Ustmani", 20);
+            person1.InfoPerson();
+
+            Console.WriteLine("---------------------------------");
+
+            Karyawan karyawan1 = new Karyawan("050704", "Dosen", "Gojo", 24);
+            karyawan1.InfoKaryawan("050704", "Dosen");
+            karyawan1.InfoPerson();
+
+            Console.ReadKey();
+        }
+    }
+}
